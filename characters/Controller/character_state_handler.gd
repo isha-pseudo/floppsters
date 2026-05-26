ddab88345637ecd9039625623552b0fcb2958439
isha-pseudo <isha44297@gmail.com>
2026-05-26 11:12:29 +0100

    updated state handler with basic functions

--- characters/Controller/character_state_handler.gd
+++ characters/Controller/character_state_handler.gd
@@ -1,11 +1,25 @@
 extends Node
+class_name CharacterStateHandler
 
+@export var character_handler: CharacterHandler
+@export var character_controller: CharacterController
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var current_squish_offset: float = 0.0
+var is_in_landing_recovery: bool = false
+var recovery_progress: float = 0.0
 
+enum State { IDLE, SQUASHING, PAUSED, RECOVERING }
+var current_state: State = State.IDLE
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+var squish_timer: float = 0.0
+var squish_target: float = 0.0
+var base_camera_y: float = 0.6
+
+func _physics_process(delta: float) -> void:
+	if character_controller == null or character_handler == null:
+		return
+	_handle_landing_detection(delta)
+	
+func _handle_landing_detection(delta: float) -> void:
 	pass
+	
